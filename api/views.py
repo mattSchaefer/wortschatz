81d232a305aef837017e2a1901c255f2576b0c9b
mattSchaefer <matthew.schaefer12@gmail.com>
2025-04-04 21:39:45 -0400

    e2e gpt translation request

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 from ninja import NinjaAPI, Schema
 from backend.services.file_reader import get_sentences_with_one_of2, get_lines, get_random_lines
+from backend.services.gpt_handler import gpt_translate
 from typing import List
 from pydantic import Field
 api = NinjaAPI()
@@ -10,8 +11,7 @@ api = NinjaAPI()
 class ReadFileRequest(Schema):
     start: int = 1
     end: int = 3
-    which_file: str = "small_file"
-    
+    which_file: str = "small_file"   
 @api.post("/read-file")
 def read_file_view(request, data: ReadFileRequest):
     file_path = f"../lookup_files/{data.which_file}.txt"
@@ -24,7 +24,6 @@ class SentenceRequest(Schema):
     words: list[str] = ["ein", "zwei", "drei"]
     limit: int = 10
     which_file: str = "deu_news_2024_10K-sentences"
-    
 @api.post("/get-sentences-with-one-of")
 def fetch_sentences_with_one_of(request, data: SentenceRequest):
     file_path = f"../lookup_files/{data.which_file}.txt"
@@ -35,18 +34,20 @@ def fetch_sentences_with_one_of(request, data: SentenceRequest):
 class RandomLinesRequest(Schema):
     num_lines: int = 20
     which_file: str = "deu_news_2024_10K-words"
-
 @api.post("/get-random-lines")
 def fetch_random_lines(request, data: RandomLinesRequest):
     file_path = f"../lookup_files/{data.which_file}.txt"
     content = get_random_lines(data.num_lines, file_path)
     return JsonResponse(content)
 
-
-
-
-
-
+class GptTranslationRequest(Schema):
+    text: str = "Kabelsalat"
+    req_type: str = "word"
+    accept_multiple: bool = True
+@api.post("/gpt-translate")
+def request_gpt_translate(request, data: GptTranslationRequest):
+    content = gpt_translate(data.text, data.req_type, data.accept_multiple)
+    return JsonResponse(content)
 
 # def get_sentences_with_one_of(request):   
 #     words = request["words"]
---
+++ backend/services/gpt_handler.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, JsonResponse
+import os
+import requests
+import json
+from django.conf import settings
+
+def gpt_translate(text, req_type, accept_multiple):
+    response_data = {}
+    prompt = "in as few words as possible,"
+    gpt_url = "https://api.openai.com/v1/responses"
+    if(accept_multiple):
+        prompt += "using an array, "
+    prompt += f"translate into English: {text}"
+    payload = json.dumps({
+        "model": "gpt-4o",
+        "input": prompt
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {os.getenv("GPT_API_KEY")}',
+    }
+    response = requests.post(gpt_url, headers=headers, data=payload)
+    response_data = response.json()
+    return response_data
